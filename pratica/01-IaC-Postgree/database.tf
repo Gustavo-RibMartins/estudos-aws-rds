b39293e802e5ec858ae6be26ad6cb5f061ec3b2a
Gustavo-RibMartins <gustavo.ribmartins@gmail.com>
2023-07-01 13:28:42 -0300

    fix db tag name

--- pratica/01-IaC-Postgree/database.tf
+++ pratica/01-IaC-Postgree/database.tf
@@ -7,8 +7,7 @@ resource "aws_db_instance" "RDS_Postgre_PizzaDB" {
   username               = "pizza123"
   password               = "pizza123"
   skip_final_snapshot    = true
-  db_subnet_group_name   = aws_subnet.subnet_priv_1a.id
   vpc_security_group_ids = [aws_security_group.sg_rds_postgres.id]
   multi_az               = true
-  tags                   = merge(local.common_tags, var.priv_subnet_1a)
+  tags                   = merge(local.common_tags, var.RDS_Postgre_PizzaDB)
 }
